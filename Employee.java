c94bea40edbed3e9cfd8344edd8a8b1af9c31a7d
ANANDITHA2005 <mattaananditha@gmail.com>
2025-05-11 07:34:28 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,9 @@
+spring.application.name=Project
+server.port=9058
+
+spring.datasource.url=jdbc:mysql://localhost:3306/klu
+spring.datasource.username=root
+spring.datasource.password=Admin
+spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+
+spring.jpa.hibernate.ddl-auto=update
